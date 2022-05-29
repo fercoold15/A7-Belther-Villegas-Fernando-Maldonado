623a253e955da4e5a89bb4721caff3518266d404
fernandomaldonado751 <fernandomaldonado751@gmail.com>
2022-05-29 09:00:24 -0600

    Se creo el modulo que calcula el MCM de dos numeros

---
+++ JS/MCM.js
@@ -0,0 +1,24 @@
+function maximoComunDivisor(){
+    var x = document.getElementById("n1");
+    var y = document.getElementById("n2");
+    var n1=x.value
+    var n2=y.value
+    let temporal;
+    while (n2 != 0) {
+        
+        temporal = n2;
+        n2 = n1 % n2;
+        n1 = temporal;
+    }
+   
+    return n1;
+};
+
+function minimoComunMultiplo(){
+    var x = document.getElementById("n1");
+    var y = document.getElementById("n2");
+    var z = document.getElementById("n3");
+    z.value = (x.value * y.value) / maximoComunDivisor();
+    x.value=null;
+    y.value=null;
+};
